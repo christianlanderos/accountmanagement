16b8710c5f62224014a92ce295be50960b3bf22e
Landeros <c_landeros_a@yahoo.com.mx>
2019-10-15 09:59:15 -0500

    initial commit. Configuring project

---
+++ src/main/java/com/unosquare/demo/accountmanagement/AccountmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.unosquare.demo.accountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountmanagementApplication.class, args);
+	}
+
+}
